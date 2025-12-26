8cfbf4b033e64b54e6645122c96f729bee2fa615
Jas <jaskq@proton.me>
2025-12-26 21:13:49 +0530

    added authentication filter

--- src/main/kotlin/com/kotlinspring/repository/UserRepository.kt
+++ src/main/kotlin/com/kotlinspring/repository/UserRepository.kt
@@ -2,35 +2,41 @@ package com.kotlinspring.repository
 
 import com.kotlinspring.dto.Role
 import com.kotlinspring.dto.User
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.stereotype.Repository
 import java.util.UUID
 
 @Repository
-class UserRepository {
+class UserRepository(
+    private val encoder: BCryptPasswordEncoder
+) {
 private val users = mutableListOf(
     User(
         id = null,
         email = "test@gmail1.com",
-        password = "test1",
+        password = encoder.encode("test1"),
         role = Role.USER
     ),
     User(
         id = null,
         email = "test@gmail2.com",
-        password = "test2",
+        password = encoder.encode("test2"),
         role = Role.ADMIN
     ),
     User(
         id = null,
         email = "test@gmail3.com",
-        password = "test3",
+        password = encoder.encode("test3"),
         role = Role.USER
     )
 )
 
-    fun save(user: User): Boolean =
-        users.add(user)
-
+    fun save(user: User): Boolean {
+        val updated = user.copy(
+            password = encoder.encode(user.password),
+        )
+        return users.add(updated)
+    }
     fun findByEmail(email: String): User? =
         users.firstOrNull { it.email == email }
 
